4a8c3a5d5411b11a0db893f506b9e8c8681d83a6
nivethagokul <umagovindan22@gmail.com>
2016-07-16 18:40:31 +0530

    Add files via upload
    palindrome or not

---
+++ hjhsfdg.java
@@ -0,0 +1,32 @@
+
+public class hjhsfdg {
+
+	
+	public static void main(String[] args) {
+		int n,n1,rev=0,rem;
+		n=Integer.parseInt(args[0]);
+				n1=n;
+		while(n>0)
+		{
+			rem=n%10;
+			rev=rev*10+rem;
+			n=n/10;
+		}
+			if(n1==rev)
+			{
+				System.out.println(+n1+ "is a palindrome");
+			}
+			else
+			{
+				System.out.println(+n1+ "is not a palindrome");
+			}
+		}
+		
+
+	
+
+
+
+	}
+
+
